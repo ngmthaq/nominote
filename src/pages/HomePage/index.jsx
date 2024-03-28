e617197f2fefb0d124f9ac4c7c5bf2698be175cd
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2024-03-28 11:15:00 +0700

    Update login UI

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,19 +1,89 @@
-import { memo, useEffect } from "react";
-import { eventBus } from "@/services/eventBus";
-import { EVENT_BUS } from "@/configs/app";
+import { memo, useId, useState } from "react";
 import classes from "./style.module.scss";
+import { isEmail } from "@/helpers/str";
 
 const HomePage = () => {
-  const handleServiceWorkerUpdated = () => {
-    alert("Reload required");
+  const emailInputId = useId();
+  const passwordInputId = useId();
+  const showPasswordInputId = useId();
+
+  const [form, setForm] = useState({ email: "", password: "", isShowPwd: false });
+
+  const isEnableSubmit = Boolean(form.email.trim()) && Boolean(form.password.trim());
+
+  const handleChangeInput = (event) => {
+    setForm((state) => ({ ...state, [event.target.name]: event.target.value }));
   };
 
-  useEffect(() => {
-    eventBus.on(EVENT_BUS.serviceWorkerUpdated, handleServiceWorkerUpdated);
-    return () => eventBus.off(EVENT_BUS.serviceWorkerUpdated, handleServiceWorkerUpdated);
-  });
+  const handleChangeCheckbox = (event) => {
+    setForm((state) => ({ ...state, isShowPwd: event.target.checked }));
+  };
 
-  return <div className={classes.homePage}>HomePage</div>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!form.email.trim()) {
+      alert("Please enter your email");
+    } else if (!form.password.trim()) {
+      alert("Please enter your password");
+    } else if (!isEmail(form.email.trim())) {
+      alert("Your email is not valid");
+    } else {
+      alert(JSON.stringify(form));
+    }
+  };
+
+  return (
+    <div className={classes.homePage}>
+      <div className={classes.wrapper}>
+        <img src="/icon-512x512.png" alt="logo" />
+        <form className="mb-4" onSubmit={handleSubmit}>
+          <h3 className="mb-4 text-center">Welcome Back</h3>
+          <div className="mb-3">
+            <label htmlFor={emailInputId} className="form-label">
+              Email Address *
+            </label>
+            <input
+              type="text"
+              name="email"
+              id={emailInputId}
+              className="form-control"
+              placeholder="Enter your email here"
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor={passwordInputId} className="form-label">
+              Password *
+            </label>
+            <input
+              type={form.isShowPwd ? "text" : "password"}
+              name="password"
+              id={passwordInputId}
+              className="form-control"
+              placeholder="Enter your password here"
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div className="form-check mb-4">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id={showPasswordInputId}
+              onChange={handleChangeCheckbox}
+            />
+            <label className="form-check-label user-select-none" htmlFor={showPasswordInputId}>
+              Show Password
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary w-100" disabled={!isEnableSubmit}>
+            Login
+          </button>
+        </form>
+        <p className="text-secondary">Copyright ©️ 2024 - {new Date().getFullYear()} | ngmthaq</p>
+      </div>
+    </div>
+  );
 };
 
 export default memo(HomePage);
